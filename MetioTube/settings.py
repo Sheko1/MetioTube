62b9f39acef85cba9a0ad23941bf480e8851786b
Martin Asenov <39015202+Sheko1@users.noreply.github.com>
2021-08-10 02:29:34 +0300

    Update settings.py

--- MetioTube/settings.py
+++ MetioTube/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['metio.hopto.org']
+ALLOWED_HOSTS = []
 
 # Application definition
 
